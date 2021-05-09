56d59770ee7df5589b2989ff94a2613a5413c466
Samet Umut Yiğitoğlu <sumutyigitoglu@hotmail.com>
2021-05-09 23:37:35 +0300

    Fixed some bugs

--- main.py
+++ main.py
@@ -2,15 +2,35 @@
 # Samet Umut Yigitoglu
 
 
-def read_file(file_path: str) -> [int]:
+class Element:
+    def __init__(self, value):
+        self.value = value
+        self.prime = is_prime(value)
+        self.max_sum = value
+
+    def get_max_sum(self) -> int:
+        return self.max_sum
+
+    def add_max_sum(self, new_sum: int):
+        self.max_sum += new_sum
+
+    def get_prime(self) -> int:
+        return self.prime
+
+    def get_value(self) -> int:
+        return self.value
+
+
+def read_file(file_path: str) -> [Element]:
     """
     Read the file given by the path
+
     :param file_path: File path
     :return: Integer array of elements
     """
     file = open(file_path)
     array = file.read().split()
-    int_array = [int(i) for i in array]
+    int_array = [Element(int(i)) for i in array]
     file.close()
     return int_array
 
@@ -18,6 +38,7 @@ def read_file(file_path: str) -> [int]:
 def is_prime(value: int) -> bool:
     """
     Checks if the given value is prime
+
     :param value: Value as an integer
     :return: Boolean value that is True if the given value is prime
     """
@@ -28,7 +49,7 @@ def is_prime(value: int) -> bool:
     return True
 
 
-def find_max_sum(triangle_array: [int]) -> int:
+def find_max_sum(triangle_array: [Element]) -> int:
     """
     This function finds the maximum sum of the orthogonal triangle according to the rules below:
         1. Start from the top and move downwards and diagonally
@@ -40,27 +61,40 @@ def find_max_sum(triangle_array: [int]) -> int:
     :param triangle_array: Array that contains all the elements of the orthogonal triangle
     :return: Maximum sum according to given rules
     """
-    max_sum = triangle_array[0]  # Top element
-    layer = 2 # Current layer
-    first_element = 1 # First element's index of the current layer
-    current_element = 0
-    while first_element < len(triangle_array):
-        if is_prime(triangle_array[first_element + current_element + 1]) and is_prime(triangle_array[first_element + current_element + 1]):
-            return max_sum
-        elif is_prime(triangle_array[first_element + current_element + 1]):
-            max_sum += triangle_array[first_element + current_element]
-        elif is_prime(triangle_array[first_element + current_element]):
-            max_sum += triangle_array[first_element + current_element + 1]
-            current_element += 1
-        elif triangle_array[first_element + current_element] > triangle_array[first_element + current_element + 1]:
-            max_sum += triangle_array[first_element + current_element]
-        else:
-            max_sum += triangle_array[first_element + current_element + 1]
-            current_element += 1
+    layer = 1  # Current layer
+    first_element = 0  # First element's index of the current layer
 
+    # Find the second last layer
+    while first_element + 2 * layer + 1 < len(triangle_array):
         first_element += layer
         layer += 1
-    return max_sum
+
+    while layer > 0:
+        for i in range(layer):
+            current_element = triangle_array[first_element + i]  # Current element
+            left_adjacent = triangle_array[first_element + i + layer]  # Current element's left diagonal adjacent
+            right_adjacent = triangle_array[first_element + i + layer + 1]  # Current element's right diagonal adjacent
+
+            # Check if the current element is not prime
+            if not current_element.get_prime():
+                # Check if both of the adjacents is not prime
+                if left_adjacent.get_prime() and right_adjacent.get_prime():
+                    pass
+                # If both of the adjacents is not prime, find which adjacent has a bigger max_sum
+                elif not left_adjacent.get_prime() and not right_adjacent.get_prime():
+                    current_element.add_max_sum(max(left_adjacent.get_max_sum(), right_adjacent.get_max_sum()))
+                # If the left adjacent is prime, add max_sum of the right adjacent
+                elif left_adjacent.get_prime():
+                    current_element.add_max_sum(right_adjacent.get_max_sum())
+                # If the right adjacent is prime, add max_sum of the left adjacent
+                elif right_adjacent.get_prime():
+                    current_element.add_max_sum(left_adjacent.get_max_sum())
+
+        # Previous layer
+        layer -= 1
+        first_element -= layer
+
+    return triangle_array[0].get_max_sum()
 
 
 if __name__ == '__main__':
